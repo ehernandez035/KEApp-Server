aba6f4cb646a7241865e78f2c65eeb7bb60e870e
Elena Hernandez <ehernandez2206@gmail.com>
2018-08-13 08:28:21 +0200

    Index is now in two languages.

--- admin/index.php
+++ admin/index.php
@@ -1,6 +1,5 @@
 <?php
 require_once "footer.php";
-$_SESSION["lang"]="eu";
 ?>
 <html>
 <head>
@@ -10,20 +9,21 @@ $_SESSION["lang"]="eu";
 
     <link rel="shortcut icon" type="image/png" href="favicon.png">
     <link rel="stylesheet" href="main.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 </head>
 <body>
 <div class="container mt-3">
-    <h1>KEApp kudeaketa</h1>
+    <h1>KEApp kudeaketa / administración</h1>
     <div class="card mt-3">
 
-        <div class="card-header">Saioa hasi</div>
+        <div class="card-header">Saioa hasi/Iniciar sesión</div>
         <div class="card-body mx-auto">
             <div>
                 <img src="logo.png" style="max-width: 250px" alt="KEApp logoa">
             </div>
             <form action="menu.php" method="post">
                 <div class="form-group">
-                    <label for="password">Pasahitza</label>
+                    <label for="password">Pasahitza/Contraseña</label>
                     <input class="form-control" type="password" name="password" id="password">
                 </div>
                 <div class="text-center">
